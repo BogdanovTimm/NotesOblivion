39dff78fb8d9e3828292bb4718d0de4773e02b49
Tim Bogdanov <101057307+BogdanovTimm@users.noreply.github.com>
2024-02-16 16:53:04 +0300

    sas script for greenplum

---
+++ 💻 IT/Work/Scripts/Cpy_Task_SAS/Cpy_Task_SAS.sql
@@ -0,0 +1,27 @@
+/* Gets all copying tasks that are longer than 1 day */
+
+select
+    task_copy_started_dt                         AS start_date,
+    task_copy_finished_dt                        AS finish_date,
+    task_copy_finished_dt - task_copy_started_dt AS time_of_copying,
+    task_status,
+    source_host_user || '@' || source_host       AS user_host_sas,
+    source_libname || '.' || target_memname      AS libname_table_sas,
+    remote_user || '@' || remote_host            AS user_host_greenplum,
+    remote_path || '.' || target_memname         AS greenplum_scheme
+FROM uat_etl_meta.CPY_TASKS_SAS
+WHERE
+    task_status IN (/*'ADDED',*/ 'TRN_RUN')
+        AND (task_copy_started_dt > current_date -  interval '1 day'
+             OR 
+             task_copy_started_dt IS NULL
+            )
+    --and source_libname  like '%A_APP%'
+    --and target_memname like '%SRVP_MR%'
+ORDER BY
+    task_status DESC,
+    table_priority,
+    source_host,
+    source_host_user,
+    copy_task_id DESC
+;
---
+++ 💻 IT/Work/Scripts/Cpy_Task_SAS/Run using SAS.md
@@ -0,0 +1,3 @@
+`/sas/bin/sas94/SASFoundation/9.4/sas ^script_name^.sas` - saves output to `^script_name^.lst` file in the script folder.
+
+`/sas/bin/sas94/SASFoundation/9.4/sas -nolog -stdio ^script_name^.sas` - prints output to stdin
